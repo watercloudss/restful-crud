3ee395d26f69a68c0484b4e65c4613e5ee078fee
lly <liuliyuan666@aliyun.com>
2019-05-24 00:20:33 +0800

    update

---
+++ src/main/java/com/springboot/study/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.springboot.study.controller;
+
+import com.springboot.study.dao.EmployeeDao;
+import com.springboot.study.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @GetMapping("/emp")
+    public String list(Model model){
+        Collection<Employee> employees=employeeDao.getAll();
+        model.addAttribute("employees",employees);
+        return "emp/list";
+    }
+}
